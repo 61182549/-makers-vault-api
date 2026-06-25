df8ecaf351b94b7df8ece87fe449e70a1afde943
Ricardo11 <61182549@certus.edu.pe>
2026-06-24 22:57:12 -0500

    Update index.js

--- index.js
+++ index.js
@@ -100,7 +100,7 @@ app.get('/api/usuarios', async (req, res) => {
 })
 
 // --- ARRANCAR SERVIDOR ---
-const PORT = 3001
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`)
 })
